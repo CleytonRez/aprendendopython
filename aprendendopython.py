46f81014430f66621f1db0f2e24af37e468c201c
Cleyton Rezende <87724338+CleytonRez@users.noreply.github.com>
2024-04-16 21:51:28 -0300

    Update aprendendopython.py

--- aprendendopython.py
+++ aprendendopython.py
@@ -38,3 +38,24 @@ convertendo_text_para_inteiro = int("50")
 print(convertendo_text_para_inteiro)
 convertendo_text_para_float = float("50.655")
 print(convertendo_text_para_float)
+
+# Listas
+lista_de_nomes = ["Jorge", "Carlos", "Vagner"]
+print(lista_de_nomes)
+
+# Adicionar novo dado no final da lista - .append
+lista_de_nomes.append("Cleber")
+print(lista_de_nomes)
+
+# Adicionar novo dado em posição especifca - .insert
+lista_de_nomes.insert(0, "Geraldinho")
+print(lista_de_nomes)
+
+# Remover dado em posição especifca - .remove
+lista_de_nomes.remove("Vagner")
+print(lista_de_nomes)
+
+# Ordenar Lista de numeros.
+idades = [20,30,32,22]
+idades.sort()
+print(idades)
